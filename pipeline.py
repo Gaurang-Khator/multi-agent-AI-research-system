7958bec1014a5fdfda435f1f8e8ca040f3da5f9f
Gaurang Khator <gaurangkhator04@gmail.com>
2026-06-04 12:28:07 +0530

    developing the pipeline

---
+++ pipeline.py
@@ -0,0 +1,13 @@
+from agents import build_search_agent , build_reader_agent , writer_chain , critic_chain
+
+def run_research_pipeline(topic : str) -> dict:
+    
+    
+
+
+
+
+
+if __name__ == "__main__":
+    topic = input("Enter a research topic:")
+    run_research_pipeline(topic)
